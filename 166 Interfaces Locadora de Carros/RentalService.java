31f9aa22a757c18160898bc16627731069531cef
Samsung <Samsung@192.168.0.191>
2025-08-17 15:34:42 -0300

    New folder created

---
+++ 166 Interfaces Locadora de Carros/RentalService.java
@@ -0,0 +1,31 @@
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerHour;
+    private Double pricePerDay;
+
+    private TaxService taxService;
+
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
+        this.pricePerHour = pricePerHour;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours / 24.0) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
